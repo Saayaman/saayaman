6c12d07b6d402c0f8ebc8c5bc789eee5bebabf0e
Ayako <ayakosayama1987@gmail.com>
2019-04-15 16:41:03 -0700

    udpate top page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './HomePage.scss';
 
-const HomePage = () => (
-  <div className="Section-one">
-    <div className="Section-one-fullBg">
-      <h1>HELLO<span className="HomePage-dot">.</span></h1>
-    </div>
-    <div className="Section-one-bg"></div>
-  </div>
-)
+class HomePage extends React.Component {
+  state = {
+    activeSide: 'right'
+  }
+  render() {
+    const { activeSide } = this.state;
+    return (
+      <div className="Section-one">
+        <div className="Section-one-fullBg">
+          <h1>HELLO<span className="HomePage-dot">.</span></h1>
+        </div>
+        {/* <div className="Section-one-bg"></div> */}
+        <div className="Homepage-airplane"></div>
+          {activeSide === 'right' ? (
+            <div className="Homepage-bg Homepage-left">
+              <div className="Homepage-vancouver">
+                <h2>To Vancouver</h2>
+                <p>Now I’m a Vancouver based<br />web designer/developer.</p>
+              </div>
+            </div>
+          ) : (
+            <div className="Homepage-bg Homepage-right">
+              <div className="Homepage-japan">
+                <h2>From Japan</h2>
+                <p>I was a graphic/web designer<br />in Tokyo.</p>
+              </div>
+            </div>
+          )}
+      </div>
+    )
+  }
+}
 
 export default HomePage;
