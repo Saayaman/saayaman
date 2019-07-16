3c35f343443394c1d5df6c2bb7b5493718864310
Ayako <ayakosayama1987@gmail.com>
2019-07-16 16:44:29 -0700

    update

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,8 +5,16 @@ import './HomePage.scss';
 import Container from '../Container/Container';
 
 class HomePage extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isActiveSideRight: true
+      })
+    }, 3000)
+  }
+
   state = {
-    isActiveSideRight: 'right'
+    isActiveSideRight: false
   }
 
   handleMouseOver = (side) => (
@@ -21,28 +29,35 @@ class HomePage extends React.Component {
         <div className="Section-one-fullBg">
           <h1>HELLO<span className="HomePage-dot">.</span></h1>
         </div>
+        <div className={cx('Section-one-green', {
+          'Section-one-green--right': isActiveSideRight
+        })} />
+
       
 
           <Container size="sm" className="Homepage-Titles">
-          <div className="Airplane">
-            <img alt="airplane" src={airplane}
-              className={cx({
-                "Homepage-japan-plane": isActiveSideRight,
-                "Homepage-vancouver-plane": !isActiveSideRight,
-              })}
-            />
-          </div>
-            <div style={{ display: 'flex', justifyContent: 'space-between', position: 'relative' }}>
+          
+            <div className="Homepage-title-wrapper">
               <span className="Homepage-vancouver-title">
-                <h2>To Vancouver</h2>
-              </span>
-              <span className="Homepage-japan-title">
                 <h2>From Japan</h2>
+                <div className="Airplane-dotted" />
+                <div className="Airplane">
+                  <img alt="airplane" src={airplane}
+                    className={cx({
+                      "Airplane-japan": !isActiveSideRight,
+                      "Airplane-vancouver": isActiveSideRight,
+                    })}
+                  />
+                </div>
+              </span>
+              
+              <span className="Homepage-japan-title">
+                <h2>To Vancouver</h2>
               </span>
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-between'}}>
-              <p>Now I’m a Vancouver based<br />web designer/developer.</p>
-              <p>I was a graphic/web designer<br />in Tokyo.</p>
+              <p className="Homepage-japan-text">I was a graphic/web designer<br />in Tokyo.</p>
+              <p className="Homepage-vancouver-text">Now I’m a Vancouver based<br /> frontend developer.</p>
             </div>
           </Container>
           
